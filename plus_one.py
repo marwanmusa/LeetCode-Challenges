8424ca2ee6ff194f39b71b2b666138143e3be197
Marwan Musa <marwanlmusa@gmail.com>
2023-01-21 18:10:40 +0700

    Plus One solution

---
+++ plus_one.py
@@ -0,0 +1,12 @@
+class Solution:
+    """
+    Task:
+    You are given a large integer represented as an integer array digits,
+    where each digits[i] is the ith digit of the integer.
+    The digits are ordered from most significant to least significant
+    in left-to-right order. The large integer does not contain any leading 0's.
+
+    Increment the large integer by one and return the resulting array of digits.
+    """
+    def plusOne(self, digits: list[int]) -> list[int]:
+        return list(int(x) for x in str(int("".join(str(num) for num in digits))+1))
