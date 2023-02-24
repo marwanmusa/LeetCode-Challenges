4c0fecfdf97237c3533b7cc988affea1a2362e39
Marwan Musa <marwanlmusa@gmail.com>
2023-02-24 21:45:37 +0700

    update 171. Excel Sheet Column Number solution

--- excel_sheet_column_number.py
+++ excel_sheet_column_number.py
@@ -4,6 +4,8 @@ class Solution:
     Given a string columnTitle that represents the column title as appears in an Excel sheet,
     return its corresponding column number.
     """
+    # First I made a list of alphabet
+    # then make pair of each word with its val
     def titleToNumber(self, columnTitle: str) -> int:
         words = [chr(i).upper() for i in range(97, 123)]
         word_val = {words[val]: val+1 for val in range(len(words))}
@@ -11,3 +13,13 @@ class Solution:
         for i in range(len(columnTitle)):
             res += word_val[columnTitle[::-1][i]]*(26**i)
         return res
+
+    # more efficient way
+    # we can use ord(alphabet)-64 instead
+    # since ord('A') == 65, ord('B') == 66 and so on
+    def titleToNumber(self, columnTitle: str) -> int:
+        columnTitle = columnTitle[::-1]
+        columnNumber = 0
+        for i in range(len(columnTitle)):
+            columnNumber += (ord(columnTitle[i]) - 64) * (26 ** i)
+        return columnNumber
