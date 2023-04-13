dc93bcec956071da185bc113f51a0039a873addb
Marwan Musa <marwanlmusa@gmail.com>
2023-04-13 13:02:17 +0700

    update 189. Rotate Array solution

--- rotate_array.py
+++ rotate_array.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Solution:
     """
     Task:
@@ -8,17 +10,22 @@ class Solution:
     Could you do it in-place with O(1) extra space?
     """
     # Method 1
-    def rotate(self, nums: list[int], k: int) -> None:
+    def rotate(self, nums: List[int], k: int) -> None:
         for _ in range(k):
             nums.insert(0, nums.pop())
 
     # Method 2
-    def rotate(self, nums: list[int], k: int) -> None:
+    def rotate(self, nums: List[int], k: int) -> None:
         break_idx = (len(nums) - k) % len(nums)
         nums[:] = nums[break_idx:] + nums[:break_idx]
 
     # Method 3
-    def rotate(self, nums: list[int], k: int) -> None:
+    def rotate(self, nums: List[int], k: int) -> None:
         k = k % len(nums)
         if k > 0:
             nums[:k], nums[k:] = nums[-k:], nums[:-k]
+
+    # Method 4
+    def rotate(self, nums: List[int], k: int) -> None:
+        k = k % len(nums)
+        nums[:] = nums[-k:] + nums[:-k]
