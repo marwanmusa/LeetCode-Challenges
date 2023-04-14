374154724f7bb493d9d9e4bb8ccde0e384be0000
Marwan Musa <marwanlmusa@gmail.com>
2023-04-14 16:56:02 +0700

    update 557. Reverse Words in a String III solution

--- reverse_word_in_string_iii.py
+++ reverse_word_in_string_iii.py
@@ -14,3 +14,14 @@ class Solution:
     # Method 2
     def reverseWords(self, s: str) -> str:
         return ' '.join(list(map(lambda word: word[::-1], s.split(' '))))
+
+
+    # Method 3
+    def reverseWords(self, s: str) -> str:
+        reversed = ""
+        s = s.split()
+        for i in range(len(s)):
+            reversed += s[i][::-1]
+            if i < len(s) - 1:
+                reversed += " "
+        return reversed
