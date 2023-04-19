40995a5bb81be877ca5178755a959a74f3ee270a
Marwan Musa <marwanlmusa@gmail.com>
2023-04-19 22:07:35 +0700

    update 1. Two Sum solution

--- two_sum.py
+++ two_sum.py
@@ -7,22 +7,13 @@ class Solution:
     and you may not use the same element twice.
 
     You can return the answer in any order
-
-    Constraints :
-    2 <= nums.length <= 104
-    -109 <= nums[i] <= 109
-    -109 <= target <= 109
-    Only one valid answer exists.
     """
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        if 2 <= len(nums) <= 10**4: # Constraint
-            if (-10)**9 <= target <= 10**9: # Constraint
-                result_dict = dict()
-                for i, value in enumerate(nums):
-                    if (-10)**9 <= nums[i] <= 10**9: # Constraint
-                        remaining_value = target - nums[i]
+        result_dict = dict()
+        for i, value in enumerate(nums):
+            remaining_value = target - nums[i]
 
-                        if remaining_value in result_dict:
-                            return [i, result_dict[remaining_value]]
+            if remaining_value in result_dict:
+                return [i, result_dict[remaining_value]]
 
-                        result_dict[value] = i
+            result_dict[value] = i
