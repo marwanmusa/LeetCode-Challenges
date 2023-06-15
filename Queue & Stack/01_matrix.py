384639c0d1da7e4477a87d9313483a3e2f9773f0
Marwan Musa <marwanlmusa@gmail.com>
2023-06-15 23:55:18 +0700

    update 542. 01 Matrix solution

--- Queue & Stack/01_matrix.py
+++ Queue & Stack/01_matrix.py
@@ -1,3 +1,4 @@
+from collection import deque
 from typing import List
 
 class Solution:
@@ -7,6 +8,7 @@ class Solution:
 
     The distance between two adjacent cells is 1.
     """
+    # bfs 1
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         h, w = len(mat), len(mat[0])
         q = []
@@ -26,5 +28,29 @@ class Solution:
                 if 0 <= nr < h and 0 <= nc < w and mat[nr][nc] == '#':
                     mat[nr][nc] = mat[row][col] + 1
                     q.append((nr, nc))
+
+        return mat
+
+
+    # bfs 2
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        m, n = len(mat), len(mat[0])
+        DIR = [0, 1, 0, -1, 0]
+
+        q = deque([])
+        for r in range(m):
+            for c in range(n):
+                if mat[r][c] == 0:
+                    q.append((r,c))
+                else:
+                    mat[r][c] = -1 # marked as not processed yet
+
+        while q:
+            r, c = q.popleft()
+            for i in range(4):
+                nr, nc = r + DIR[i], c + DIR[i + 1]
+                if nr < 0 or nr == m or nc < 0 or nc == n or mat[nr][nc] != -1: continue
+                mat[nr][nc] = mat[r][c] + 1
+                q.append((nr, nc))
 
         return mat
