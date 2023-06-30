cfcb6cf35621e9e4ffd9503a4c84cbbf50e254e6
Marwan Musa <marwanlmusa@gmail.com>
2023-06-30 23:49:11 +0700

    update 268. Missing Numbers solution

--- missing_numbers.py
+++ missing_numbers.py
@@ -6,5 +6,13 @@ class Solution:
     Given an array nums containing n distinct numbers in the range [0, n],
     return the only number in the range that is missing from the array.
     """
+    # sum
     def missingNumber(self, nums: List[int]) -> int:
         return sum(range(len(nums)+1))-sum(nums)
+
+    # or
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        # Get sum of complete series(Gaussian formula)
+        # and find the difference between sum of given series
+        return ((n * (n+1)) // 2 ) - sum(nums) 
