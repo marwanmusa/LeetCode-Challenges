13e43d2ebe7a2a4496878ba2562bc645e0efce90
Marwan Musa <marwanlmusa@gmail.com>
2023-07-01 09:42:24 +0700

    update 268. Missing Numbers solution

--- missing_numbers.py
+++ missing_numbers.py
@@ -15,4 +15,51 @@ class Solution:
         n = len(nums)
         # Get sum of complete series(Gaussian formula)
         # and find the difference between sum of given series
-        return ((n * (n+1)) // 2 ) - sum(nums) 
+        return ((n * (n+1)) // 2 ) - sum(nums)
+
+    # bitwise
+    def missingNumber(self, nums: List[int]) -> int:
+
+        result = 0
+
+        for counter, value in enumerate(nums):
+
+            result ^= counter+1 # XOR result with numbers from the complete series
+            result ^= value # XOR with the numbers given in num series
+
+        return result
+
+        """
+        # Simple break down of above XOR solution
+
+        numsList = [3,0,1] # Missing number is 2
+
+        result = 0
+
+        #XOR result with complete number sequence
+        for number in range(len(numsList) + 1 ) : # 0, 1, 2 ,3
+                result ^= number
+
+        # Essentially now result = ( 0 ^ 0 ^ 1 ^ 2 ^ 3)
+
+        #XOR result with values in nums
+        for num in numsList : # 3,0,1
+                result ^= num
+
+        # result = ( 0 ^ 0 ^ 1 ^ 2 ^ 3) ^ ( 3 ^ 0 ^ 1)
+
+        # XOR of same values cancel each other out
+
+        # result = (0) ^ (0 ^ 0)  ^ (1^1) ^ (2) ^ (3^3)
+        # result =  0 ^ 0 ^ 0 ^ 2 ^ 0
+        # result = 2
+
+        return result # 2
+        """
+
+    # shortened bitwise solution
+    def missingNumber(self, nums: List[int]) -> int:
+        res = len(nums)
+        for i in range(len(nums)):
+            res ^= i ^ nums[i]
+        return res
