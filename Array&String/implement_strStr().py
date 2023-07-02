517cf89dde8d320a65b51ea9f4026d09aeb6d0b8
Marwan Musa <marwanlmusa@gmail.com>
2023-07-02 23:50:18 +0700

    regrouping solutions

---
+++ Array&String/implement_strStr().py
@@ -0,0 +1,9 @@
+class Solution:
+    """
+    Task:
+    Given two strings needle and haystack,
+    return the index of the first occurrence of needle in haystack,
+    or -1 if needle is not part of haystack.
+    """
+    def strStr(self, haystack: str, needle: str) -> int:
+        return haystack.find(needle)
