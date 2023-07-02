3436074bdb6e27993d9ac6ef3715329e74a51c87
Marwan Musa <marwanlmusa@gmail.com>
2023-07-02 23:55:19 +0700

    add KMP implementation

--- Array&String/implement_strStr().py
+++ Array&String/implement_strStr().py
@@ -7,3 +7,40 @@ class Solution:
     """
     def strStr(self, haystack: str, needle: str) -> int:
         return haystack.find(needle)
+
+
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        for i in range(len(haystack) - len(needle)+1):
+            if haystack[i:i+len(needle)] == needle:
+                return i
+        return -1
+
+
+    # KMP algorithm
+    def strStr(self, haystack, needle):
+
+        def kmp(str_):
+                b, prefix = 0, [0]
+                for i in range(1, len(str_)):
+                    while b > 0 and str_[i] != str_[b]:
+                        b = prefix[b - 1]
+                    if str_[b] == str_[i]:
+                        b += 1
+                    else:
+                        b = 0
+                    prefix.append(b)
+                return prefix
+
+        str_ = kmp(needle + '#' + haystack)
+        n = len(needle)
+        if n == 0:
+            return n
+        for i in range(n + 1, len(str_)):
+            if str_[i] == n:
+                return i - 2 * n
+        return -1
