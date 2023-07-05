6d5cfc530d5c6eb7b76c002b3d8b88a3e391a374
Marwan Musa <marwanlmusa@gmail.com>
2023-07-05 18:48:45 +0700

    add KMP implementation

--- Array&String/find_the_index_of_the_first_occurrence_in_a_string.py
+++ Array&String/find_the_index_of_the_first_occurrence_in_a_string.py
@@ -38,3 +38,28 @@ class Solution:
     # Method 3 using built-in function
     def strStr(self, haystack: str, needle: str) -> int:
         return haystack.find(needle)
+
+
+    # KMP algorithm
+    def strStr(self, haystack, needle):
+
+        def kmp(str_):
+                b, prefix = 0, [0]
+                for i in range(1, len(str_)):
+                    while b > 0 and str_[i] != str_[b]:
+                        b = prefix[b - 1]
+                    if str_[b] == str_[i]:
+                        b += 1
+                    else:
+                        b = 0
+                    prefix.append(b)
+                return prefix
+
+        str_ = kmp(needle + '#' + haystack)
+        n = len(needle)
+        if n == 0:
+            return n
+        for i in range(n + 1, len(str_)):
+            if str_[i] == n:
+                return i - 2 * n
+        return -1
