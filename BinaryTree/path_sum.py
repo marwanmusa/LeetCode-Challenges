d908b62bc68d86cca65d4e2c045f716057c8ef77
Marwan Musa <marwanlmusa@gmail.com>
2023-07-14 23:52:01 +0700

    add dfs approach

--- BinaryTree/path_sum.py
+++ BinaryTree/path_sum.py
@@ -38,3 +38,19 @@ class Solution:
         if root.right != None:
             ans = ans or self.hasPathSum(root.right, sub_sum)
         return ans
+
+
+    # dfs
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        res = []
+        self.dfs(root, targetSum, res)
+        return any(res)
+
+    def dfs(self, root, target, res):
+        if root:
+            if not root.left and not root.right and root.val == target:
+                res.append(True)
+            if root.left:
+                self.dfs(root.left, target-root.val, res)
+            if root.right:
+                self.dfs(root.right, target-root.val, res)
