9c41dafce3352f09eaab834b1aab5fd8ca956ac0
Marwan Musa <marwanlmusa@gmail.com>
2023-07-25 21:16:47 +0700

    add iterative aproach

--- BinaryTree/lowest_common_ancestor_of_a_binary_tree.py
+++ BinaryTree/lowest_common_ancestor_of_a_binary_tree.py
@@ -29,3 +29,23 @@ class Solution:
         left, right = (self.lowestCommonAncestor(kid, p, q)
                       for kid in (root.left, root.right))
         return root if left and right else left or right
+
+    # iterative approach
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        stack = [root]
+        parent = {root: None}
+        while p not in parent or q not in parent:
+            node = stack.pop()
+            if node.left:
+                parent[node.left] = node
+                stack.append(node.left)
+            if node.right:
+                parent[node.right] = node
+                stack.append(node.right)
+        ancestors = set()
+        while p:
+            ancestors.add(p)
+            p = parent[p]
+        while q not in ancestors:
+            q = parent[q]
+        return q
