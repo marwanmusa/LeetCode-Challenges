6c35d96827e7bd206f4d6da1db337a9d9285cb6c
Marwan Musa <marwanlmusa@gmail.com>
2023-07-29 00:03:36 +0700

    update solution

--- add_strings.py
+++ add_strings.py
@@ -1,3 +1,6 @@
+import sys
+sys.set_int_max_str_digits(0)
+
 class Solution:
     """
     Task:
@@ -7,3 +10,12 @@ class Solution:
     You must also not convert the inputs to integers directly.
     """
     def addStrings(self, num1: str, num2: str) -> str:
+        l1, l2 = len(num1), len(num2)
+        a = b = 0
+        for i in range(l1):
+            r = ord(num1[i]) - 48
+            a = a*10 + r
+        for i in range(l2):
+            s = ord(num2[i]) - 48
+            b = b*10 + s
+        return str(a + b)
