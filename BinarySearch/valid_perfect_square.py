bc17899b65771e40057475f2128a3b098d04f89d
Marwan Musa <marwanlmusa@gmail.com>
2023-08-15 22:57:39 +0700

    add method

--- BinarySearch/valid_perfect_square.py
+++ BinarySearch/valid_perfect_square.py
@@ -19,3 +19,16 @@ class Solution:
             else:
                 r = mid - 1
         return False
+    
+    # or
+    def isPerfectSquare(self, num: int) -> bool:
+        l, r = 1, num/2
+        while l < r:
+            mid = l + (r-l)//2
+            if mid*mid == num:
+                return True
+            elif mid*mid < num:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return l*l==num
