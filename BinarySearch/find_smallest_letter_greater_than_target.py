0ef5d1979965156916548c0e2246ad21a6ad60a8
Marwan Musa <marwanlmusa@gmail.com>
2023-08-17 23:55:37 +0700

    add binary search approach

--- BinarySearch/find_smallest_letter_greater_than_target.py
+++ BinarySearch/find_smallest_letter_greater_than_target.py
@@ -1,9 +1,22 @@
 from typing import List
 
 class Solution:
+    # brute force
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-        # brute force
         for char in letters:
             if char > target:
                 return char
         return letters[0]
+    
+
+    # binary search
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        s = -1
+        e = len(letters) - 1
+        while s+1 < e:
+            m = (s + e)//2
+            if letters[m] > target:
+                e = m
+            else:
+                s = m
+        return letters[e] if letters[e] > target else letters[0]
