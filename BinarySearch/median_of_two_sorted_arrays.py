9616804362f17da514990e79febd60e1b214336d
Marwan Musa <marwanlmusa@gmail.com>
2023-08-30 15:09:13 +0700

    add Approach 3: A Better Binary Search

--- BinarySearch/median_of_two_sorted_arrays.py
+++ BinarySearch/median_of_two_sorted_arrays.py
@@ -74,3 +74,29 @@ class Solution:
             return solve(n // 2, 0, na - 1, 0, nb - 1)
         else:
             return (solve(n // 2 - 1, 0, na - 1, 0, nb - 1) + solve(n // 2, 0, na - 1, 0, nb - 1)) / 2
+        
+    # Approach 3: A Better Binary Search
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        if len(nums1) > len(nums2):
+            return self.findMedianSortedArrays(nums2, nums1)
+        m, n = len(nums1), len(nums2)
+        left, right = 0, m
+
+        while left <= right:
+            partitionA = (left + right) // 2
+            partitionB = (m + n + 1) //2 - partitionA
+
+            maxLeftA = float('-inf') if partitionA == 0 else nums1[partitionA - 1]
+            minRightA = float('inf') if partitionA == m else nums1[partitionA]
+            maxLeftB = float('-inf') if partitionB == 0 else nums2[partitionB - 1]
+            minRightB = float('inf') if partitionB == n else nums2[partitionB]
+
+            if maxLeftA <= minRightB and maxLeftB <= minRightA:
+                if not (m + n) & 1:
+                    return (max(maxLeftA, maxLeftB) + min(minRightA, minRightB)) / 2
+                else:
+                    return max(maxLeftA, maxLeftB)
+            elif maxLeftA > minRightB:
+                right = partitionA - 1
+            else:
+                left = partitionA + 1
