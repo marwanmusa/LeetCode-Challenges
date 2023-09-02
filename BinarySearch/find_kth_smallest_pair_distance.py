c755a634e1840a7bcf0e59fbfd8edabf7becec5e
Marwan Musa <marwanlmusa@gmail.com>
2023-09-02 23:27:50 +0700

    add Approach 2: Binary Search + Prefix Sum

--- BinarySearch/find_kth_smallest_pair_distance.py
+++ BinarySearch/find_kth_smallest_pair_distance.py
@@ -19,3 +19,38 @@ class Solution:
             if nei+1 < n:
                 heappush(heap, (nums[nei+1] - nums[root], root, nei+1))
         return d
+    
+    # Approach 2: Binary Search + Prefix Sum
+    def smallestDistancePair(self, nums: List[int], k: int) -> int:
+        def possible(guess):
+            #Is there k or more pairs with distance <= guess?
+            return sum(prefix[min(x + guess, W)] - prefix[x] + multiplicity[i]
+                       for i, x in enumerate(nums)) >= k
+        
+        nums.sort()
+        W = nums[-1]
+
+        # multiplicity[i] = number of nums[j] == nums[i] (j < i)
+        multiplicity  = [0] * len(nums)
+        for i, x in enumerate(nums):
+            if i and x == nums[i-1]:
+                multiplicity[i] = 1 + multiplicity[i-1]
+        
+        # prefix[v] = number of values <= v
+        prefix = [0] * (W + 1)
+        l = 0
+        for i in range(len(prefix)):
+            while l < len(nums) and nums[l] == i:
+                l += 1
+            prefix[i] = l
+
+        lo = 0
+        hi = nums[-1] - nums[0]
+        while lo < hi:
+            mi = lo + (hi-lo)//2
+            if possible(mi):
+                hi = mi
+            else:
+                lo = mi + 1
+
+        return lo
