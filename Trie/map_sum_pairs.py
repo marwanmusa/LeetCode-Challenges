eb9efb708bf559a4444fbcc9771251a559186a5b
Marwan Musa <marwanlmusa@gmail.com>
2023-09-10 22:18:05 +0700

    add Prefix Hashmap approach

--- Trie/map_sum_pairs.py
+++ Trie/map_sum_pairs.py
@@ -11,6 +11,9 @@ Implement the MapSum class:
     - int sum(string prefix) Returns the sum of all the pairs' value whose key starts with the prefix.
 """
 
+from collections import Counter
+
+
 # Approach 1: Brute-Force
 class MapSum(object):
     def __init__(self):
@@ -22,3 +25,19 @@ class MapSum(object):
     def sum(self, prefix):
         return sum(val for key, val in self.map.items()
                    if key.startswith(prefix))
+    
+# Approach 2: Prefix Hashmap
+class MapSum(object):
+    def __init__(self):
+        self.map = {}
+        self.score = Counter()
+
+    def insert(self, key, val):
+        delta = val - self.map.get(key, 0)
+        self.map[key] = val
+        for i in range(len(key) + 1):
+            prefix = key[:i]
+            self.score[prefix] += delta
+    
+    def sum(self, prefix):
+        return self.score[prefix]
