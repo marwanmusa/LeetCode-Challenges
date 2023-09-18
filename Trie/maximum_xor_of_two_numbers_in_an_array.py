bd0491681fd8613024c6c5e360f1e91d85044ac1
Marwan Musa <marwanlmusa@gmail.com>
2023-09-18 17:06:51 +0700

    fix typo

--- Trie/maximum_xor_of_two_numbers_in_an_array.py
+++ Trie/maximum_xor_of_two_numbers_in_an_array.py
@@ -18,6 +18,6 @@ class Solution:
         ans = 0
         for i in range(32)[::-1]:
             ans <<= 1
-            prefixes = {num >> i for num in nums[i]}
+            prefixes = {num >> i for num in nums}
             ans += any(ans^1 ^ p in prefixes for p in prefixes)
         return ans
