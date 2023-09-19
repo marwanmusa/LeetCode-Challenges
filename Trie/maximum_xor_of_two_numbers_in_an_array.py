fc66d606c42898e30b6f0e5468200dbdeb33ca57
Marwan Musa <marwanlmusa@gmail.com>
2023-09-19 15:33:49 +0700

    modified previous solution approach : bit operation

--- Trie/maximum_xor_of_two_numbers_in_an_array.py
+++ Trie/maximum_xor_of_two_numbers_in_an_array.py
@@ -12,7 +12,7 @@ class Solution:
             a, b = pair
             x = max(a^b, x)
         return x
-    
+
     # Approach 2: Bit Operation
     def findMaximumXOR(self, nums: list[int]) -> int:
         ans = 0
@@ -20,4 +20,15 @@ class Solution:
             ans <<= 1
             prefixes = {num >> i for num in nums}
             ans += any(ans^1 ^ p in prefixes for p in prefixes)
+        return ans
+
+    # Approach 2: Modification I
+    def findMaximumXOR(self, nums: list[int]) -> int:
+        ans = 0
+        for i in reversed(range(32)):
+            prefixes = set([x >> i for x in nums])
+            ans <<= 1
+            candidate = ans + 1
+            if any(candidate^p in prefixes for p in prefixes):
+                ans = candidate
         return ans
