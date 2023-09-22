4229f4cbf4b9e9adcd02ec810d67b09fe9378821
Marwan Musa <marwanlmusa@gmail.com>
2023-09-22 23:49:41 +0700

    add another approach : backtracking with Trie

--- Trie/word_search_ii.py
+++ Trie/word_search_ii.py
@@ -44,3 +44,44 @@ and I don't need to check borders because board.get just returns "None" if I req
 After this preparation, I just take the tree and recursively dive with it into each board position.
 Similar to how you'd search a single word, but with the tree instead.
 """
+
+# Another Solution : Backtracking with Trie
+
+from collections import defaultdict
+from typing import List
+
+class TrieNode:
+    def __init__(self):
+        self.children = defaultdict(TrieNode)
+        self.word = None
+
+    def addWord(self, word):
+        cur = self
+        for c in word:
+            cur = cur.children[c]
+        cur.word = word
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        m, n = len(board), len(board[0])
+        DIR = [0, 1, 0, -1, 0]
+        trieNode = TrieNode()
+        ans = []
+        for word in words:
+            trieNode.addWord(word)
+
+        def dfs(r, c, cur):
+            if r < 0 or r == m or c < 0 or c == n or board[r][c] not in cur.children: return
+            orgChar = board[r][c]
+            cur = cur.children[orgChar]
+            board[r][c] = '#'  # Mark as visited
+            if cur.word != None:
+                ans.append(cur.word)
+                cur.word = None  # Avoid duplication!
+            for i in range(4): dfs(r + DIR[i], c + DIR[i + 1], cur)
+            board[r][c] = orgChar  # Restore to org state
+
+        for r in range(m):
+            for c in range(n):
+                dfs(r, c, trieNode)
+        return ans
