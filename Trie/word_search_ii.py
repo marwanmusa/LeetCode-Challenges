72d5603276ebc59ebad780d4c5bde4ca051a9021
Marwan Musa <marwanlmusa@gmail.com>
2023-09-27 23:23:11 +0700

    add backtracking 2 approach

--- Trie/word_search_ii.py
+++ Trie/word_search_ii.py
@@ -87,6 +87,57 @@ class Solution:
         return 
     
 
+# Backtracking II
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isWord = False
+        self.word = ""
+        
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        if not board or len(board) == 0 or not words or len(words) == 0:
+            return []
+        
+        root = TrieNode()
+        
+        for word in words:
+            temp = root
+            for letter in word:
+                if letter in temp.children:
+                    temp = temp.children[letter]
+                else:
+                    currWord = temp.word
+                    temp.children[letter] = TrieNode()
+                    temp = temp.children[letter]
+                    temp.word = currWord + letter
+            temp.isWord = True
+        res = []
+        
+        def backtrack(r, c, node):
+            if node.isWord:
+                res.append(node.word)
+                node.isWord = False
+            if r < 0 or r > len(board)-1 or c < 0 or c > len(board[r])-1:
+                return
+            if board[r][c] not in node.children:
+                return
+            
+            tmpLetter = board[r][c]
+            board[r][c] = '$'
+            backtrack(r+1, c, node.children[tmpLetter])
+            backtrack(r-1, c, node.children[tmpLetter])
+            backtrack(r, c+1, node.children[tmpLetter])
+            backtrack(r, c-1, node.children[tmpLetter])
+            board[r][c] = tmpLetter
+        
+        for r in range(len(board)):
+            for c in range(len(board[r])):
+                if board[r][c] in root.children:
+                    backtrack(r, c, root)
+        return res
+    
+
 # Another DFS approach
 class TrieNode:
     def __init__(self):
