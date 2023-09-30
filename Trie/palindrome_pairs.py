f872982d1d749a831c47b6655746e272ce81ea85
Marwan Musa <marwanlmusa@gmail.com>
2023-09-30 23:58:54 +0700

    add 336. Palindrome Pairs solution

---
+++ Trie/palindrome_pairs.py
@@ -0,0 +1,34 @@
+"""
+You are given a 0-indexed array of unique strings words.
+
+A palindrome pair is a pair of integers (i, j) such that:
+    - 0 <= i, j < words.length,
+    - i != j, and
+    - words[i] + words[j] (the concatenation of the two strings) is a palindrome.
+Return an array of all the palindrome pairs of words.
+
+You must write an algorithm with O(sum of words[i].length) runtime complexity.
+"""
+
+
+class Solution:
+    def palindromePairs(self, words: list[str]) -> list[list[int]]:
+        def is_palindrome(check):
+            return check == check[::-1]
+
+        words = {word: i for i, word in enumerate(words)}
+        valid_pals = []
+        for word, k in words.items():
+            n = len(word)
+            for j in range(n+1):
+                pref = word[:j]
+                suf = word[j:]
+                if is_palindrome(pref):
+                    back = suf[::-1]
+                    if back != word and back in words:
+                        valid_pals.append([words[back],  k])
+                if j != n and is_palindrome(suf):
+                    back = pref[::-1]
+                    if back != word and back in words:
+                        valid_pals.append([k, words[back]])
+        return valid_pals
