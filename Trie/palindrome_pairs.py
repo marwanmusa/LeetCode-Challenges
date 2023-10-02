4410c95f65f2e2ee8b37a383c86c1eef8dda58ca
Marwan Musa <marwanlmusa@gmail.com>
2023-10-02 14:33:10 +0700

    add hashmap approach

--- Trie/palindrome_pairs.py
+++ Trie/palindrome_pairs.py
@@ -40,3 +40,41 @@ class Solution:
                     if back != word and back in words:
                         valid_pals.append([k, words[back]])
         return valid_pals
+    
+    # Hashmap
+    def palindromePairs(self, words: list[str]) -> list[list[int]]:
+        # keep a hashmap of word to its index
+        idx_map = {}
+        for i, word in enumerate(words):
+            idx_map[word] = i
+            
+        res = set()
+        for i, word in enumerate(words):
+            if not word:
+                # we don't process empty string by itself
+                continue
+            
+            # generate all possible LHS that would form
+            # a palindrome with current word
+            for j in range(len(word)):
+                current = word[:j]
+                target = word[j:][::-1]
+                if current == current[::-1] and target != word and target in idx_map:
+                    res.add((idx_map[target], i))
+                    
+            # generate all possible RHS that would form
+            # a palindrome with current word
+            for j in range(len(word), -1, -1):
+                current = word[j:]
+                target = word[:j][::-1]
+                if current == current[::-1] and target != word and target in idx_map:
+                    res.add((i, idx_map[target]))
+                    
+            # check if current word is already a palindrome and
+            # if we have an empty string in our map
+            if word == word[::-1] and "" in idx_map:
+                idx = idx_map[""]
+                res.add((i, idx))
+                res.add((idx, i))
+
+        return list(res)
