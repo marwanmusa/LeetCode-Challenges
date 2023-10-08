d4dd2b15228e439fdf564b4bf11c65f65f3f803a
Marwan Musa <marwanlmusa@gmail.com>
2023-10-08 23:51:31 +0700

    add Insertion-sort approach

--- Recursion-2/sort_an_array.py
+++ Recursion-2/sort_an_array.py
@@ -43,3 +43,13 @@ class Solution:
                     nums[j], nums[j+1] = nums[j+1], nums[j]
         return nums
     
+    # Insertion-sort TLE
+    def sortArray(self, nums: list[int]) -> list[int]:
+        for i in range(1, len(nums)):
+            key = nums[i]
+            j = i-1
+            while j >= 0 and key < nums[j]:
+                nums[j+1] = nums[j]
+                j -= 1
+            nums[j+1] = key
+        return nums
