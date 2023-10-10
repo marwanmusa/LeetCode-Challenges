c5b56276fe1fda1b36949c53d42b25588e7f84ea
Marwan Musa <marwanlmusa@gmail.com>
2023-10-10 12:17:43 +0700

    add Merge-Sprt approach vers-2

--- Recursion-2/sort_an_array.py
+++ Recursion-2/sort_an_array.py
@@ -33,6 +33,37 @@ class Solution:
             return ans
         
         return merge(l_nums, r_nums)
+    
+    # Merge-sort 2
+    def sortArray(self, nums: list[int]) -> list[int]:
+        if len(nums) > 1:
+            mid = len(nums) // 2
+            l = nums[:mid]
+            r = nums[mid:]
+
+            self.sortArray(l)
+            self.sortArray(r)
+
+            i = j = k = 0
+
+            while i < len(l) and j < len(r):
+                if l[i] < r[j]:
+                    nums[k] = l[i]
+                    i += 1
+                else:
+                    nums[k] = r[j]
+                    j += 1
+                k += 1
+            
+            while i < len(l):
+                nums[k] = l[i]
+                i += 1
+                k += 1
+            while j < len(j):
+                nums[k] = r[j]
+                j += 1
+                k += 1
+        return nums
 
     # Buble-sort TLE
     def sortArray(self, nums: list[int]) -> list[int]:
