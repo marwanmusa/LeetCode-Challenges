7bf4af1027af71dcdb335cd32fbb553c3c10d46e
Marwan Musa <marwanlmusa@gmail.com>
2023-10-11 19:14:23 +0700

    add heap sort appraoch

--- Recursion-2/sort_an_array.py
+++ Recursion-2/sort_an_array.py
@@ -104,3 +104,30 @@ class Solution:
 
         helper(0, len(nums)-1)
         return nums
+    
+    # Heap-sort
+    def sortArray(self, nums: list[int]) -> list[int]:
+        def heapify(nums, n, i):
+            l = 2*i+1
+            r = 2*i+2
+
+            largest = i
+            if l < n and nums[largest] < nums[l]:
+                largest = l
+
+            if r < n and nums[largest] > nums[r]:
+                largest = r
+
+            if largest != i:
+                nums[i], nums[largest] = nums[largest], nums[i]
+
+                heapify(nums, n, largest)
+
+        n = len(nums)
+
+        for i in range(n//2+1)[::-1]:
+            heapify(nums, n, i)
+
+        for i in range(n)[::-1]:
+            nums[i], nums[0] = nums[0], nums[i]
+            heapify(nums, i, 0)
