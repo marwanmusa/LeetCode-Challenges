a050c767d4ddd019aebcc8e74a895bce23880feb
Marwan Musa <marwanlmusa@gmail.com>
2023-10-19 19:23:34 +0700

    delete unused space

--- Recursion-2/search_a_2d_matrix.py
+++ Recursion-2/search_a_2d_matrix.py
@@ -53,12 +53,12 @@ class Solution:
     def searchMatrix(self, matrix: list[list[int]], target: int) -> bool:
         """
     O(log(mn)) time, O(logmn) space, because we discard 1/4 of the matrix each time.
-    
+
     Recursive binary search midpoint of matrix. Given co-ordiantes (r_low, r_high, c_low, c_high)
     inclusive of a square submatrix, and midpoint r_mid=r_low+(r_high-r_low)//2, c_mid=c_low+(c_high-c_low)//2,
-    
+
     Use helper function search(r_low, r_high, c_low, c_high) on the co-ordinates of a sub-matrix
-    
+
     1. Check (r_low, r_high, c_low, c_high) is valid, i.e. 0<=r_low<=r_high<=R-1, 0<=c_low<=c_high<=C-1.
     If not return False.
     2. If matrix[r_mid][c_mid]==target, return True
@@ -80,12 +80,12 @@ class Solution:
             c_mid=c_low+(c_high-c_low)//2
             if matrix[r_mid][c_mid]==target:
                 return True
-            
+
             elif matrix[r_mid][c_mid]>target: #discard bottom right
                 tl=(r_low, r_mid-1, c_low, c_mid)
                 tr=(r_low, r_mid-1, c_mid+1, c_high)
                 bl=(r_mid, r_high, c_low, c_mid-1)
-                
+
                 return search_sub(*tl) or search_sub(*tr) or search_sub(*bl)
             elif matrix[r_mid][c_mid]<target: #discard top left
                 tr=(r_low, r_mid, c_mid+1, c_high)
@@ -93,5 +93,5 @@ class Solution:
                 bl=(r_mid+1, r_high, c_low, c_mid-1)
 
                 return search_sub(*tr) or search_sub(*br) or search_sub(*bl)
-            
+
         return search_sub(0, R-1, 0, C-1)
