404e379e3f0d7d4326f321dce1844bff79a2a96f
Marwan Musa <marwanlmusa@gmail.com>
2023-10-21 22:43:37 +0700

    add another approach #linear-search

--- Recursion-2/search_a_2d_matrix.py
+++ Recursion-2/search_a_2d_matrix.py
@@ -94,3 +94,15 @@ class Solution:
                 return search_sub(*tr) or search_sub(*br) or search_sub(*bl)
 
         return search_sub(0, R-1, 0, C-1)
+    
+    # Linear Search from Top-Right Corner
+    def searchMatrix(matrix, target):
+        m, n = len(matrix), len(matrix) and len(matrix[0])
+        r, c = 0, n-1
+        while r < m and c >= 0:
+            if target > matrix[r][c]:
+                r += 1
+            elif target < matrix[r][c]:
+                c -= 1
+            else: return True
+        return False
