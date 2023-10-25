6156ae043858a870972e427ec4fd92ae7ab8ed7b
Marwan Musa <marwanlmusa@gmail.com>
2023-10-25 18:20:20 +0700

    add quick-sort steps

--- Recursion-2/quick_sort_template.py
+++ Recursion-2/quick_sort_template.py
@@ -1,3 +1,25 @@
+# Quick Sort Algorithm
+"""
+In detail, given a list of values to sort, the quick sort algorithm works in the following steps:
+
+First, it selects a value from the list, which serves as a pivot value to divide the list into two sublists.
+One sublist contains all the values that are less than the pivot value,
+while the other sublist contains the values that are greater than or equal to the pivot value.
+This process is also called partitioning. The strategy of choosing a pivot value can vary.
+Typically, one can choose the first element in the list as the pivot, or randomly pick an element from the list.
+
+After the partitioning process, the original list is then reduced into two smaller sublists. We then recursively sort the two sublists.
+
+After the partitioning process, we are sure that all elements in one sublist are less or equal than any element in another sublist.
+Therefore, we can simply concatenate the two sorted sublists that we obtain in step [2] to obtain the final sorted list. 
+The base cases of the recursion in step [2] are either when the input list is empty or contains only a single element.
+In either case, the input list can be considered as sorted already.
+
+As one can see, the essential idea of the quick sort algorithm is the partitioning process, 
+which elegantly reduces the problems into smaller scale and meanwhile moves towards the final solution,
+i.e. after each partitioning, the overall values become more ordered. 
+"""
+
 def quicksort(arr: list) -> list:
     """
     Sorts an array in the ascending order in O(n log n) time
