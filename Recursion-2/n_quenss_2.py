a9064e6ea86129b15d404de35e691453a1a4ea24
Marwan Musa <marwanlmusa@gmail.com>
2023-10-31 16:02:08 +0700

    add shorter solution from previous version

--- Recursion-2/n_quenss_2.py
+++ Recursion-2/n_quenss_2.py
@@ -4,16 +4,17 @@ class Solution:
 
     Given an integer n, return the number of distinct solutions to the n-queens puzzle.
     """
+    # Approach 1
     def totalNQueens(self, n: int) -> int:
         diag1 = set()
         diag2 = set()
         used_cols = set()
         return self.helper(n, diag1, diag2, used_cols, 0)
-    
+
     def helper(self, n, diag1, diag2, used_cols, row):
         if row == n:
             return 1
-        
+
         solutions = 0
 
         for col in range(n):
@@ -28,5 +29,26 @@ class Solution:
 
             diag1.remove(row + col)
             diag2.remove(row - col)
+            used_cols.remove(col)
+        return solutions
+
+    # Approach 1 shorter
+    def totalNQueens(self, n: int, diag1: set = set(), diag2: set = set(), used_cols: set = set(), row: int = 0) -> int:
+        if row == n: return 1
+
+        solutions = 0
+
+        for col in range(n):
+            if (row+col) in diag1 or (row-col) in diag2 or col in used_cols:
+                continue
+
+            diag1.add(row+col)
+            diag2.add(row-col)
+            used_cols.add(col)
+
+            solutions += self.totalNQueens(n, diag1, diag2, used_cols, row+1)
+
+            diag1.remove(row+col)
+            diag2.remove(row-col)
             used_cols.remove(col)
         return solutions
