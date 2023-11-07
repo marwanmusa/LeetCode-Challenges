3321deca529c21f46259516ef48bdae544841d08
Marwan Musa <marwanlmusa@gmail.com>
2023-11-07 22:05:30 +0700

    update typing

--- Recursion-2/n_quenss_2.py
+++ Recursion-2/n_quenss_2.py
@@ -58,7 +58,6 @@ class Solution:
         def dfs(board, i, c1, c2, c3):
             if i == n:
                 self.ans += 1
-            
             for j in range(n):
                 if c1 & 1<<j or c2 & 1<<i-j+n or c3 & 1<<i+j:
                     continue
