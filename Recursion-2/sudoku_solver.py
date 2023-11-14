12423c289efefa1440b6f07eb5df7594dfc8b605
Marwan Musa <marwanlmusa@gmail.com>
2023-11-14 19:29:36 +0700

    update solution

--- Recursion-2/sudoku_solver.py
+++ Recursion-2/sudoku_solver.py
@@ -81,12 +81,29 @@ class Solution:
         self.initState()
         self.solve()
 
+    def findUnassigned(self):
+        for row in range(9):
+            for col in range(9):
+                if self.board[row][col] == '.':
+                    return row, col
+        return -1, -1
+        
     def initState(self):
         for row in range(9):
             for col in range(9):
                 if self.board[row][col] != ".":
                     self.state[self.board[row][col]] += 1
 
+    def isSafe(self, row, col, ch):
+        rowSafe = all(self.board[row][_] != ch for _ in range(9))
+        colSafe = all(self.board[_][col] != ch for _ in range(9))            
+        squareSafe = all(self.board[r][c] != ch for r in self.getRange(row) for c in self.getRange(col))
+        return rowSafe and colSafe and squareSafe
+    
+    def getRange(self, x):
+        x -= x % 3
+        return range(x, x + 3)
+                    
     def solve(self):
         row, col = self.findUnassigned()
         if row == -1 and col == -1:
