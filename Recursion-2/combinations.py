606b8715b2e4b1ec01c7d4b7dc6eb87dea82cc58
Marwan Musa <marwanlmusa@gmail.com>
2023-11-17 23:48:01 +0700

    add iterative approach

--- Recursion-2/combinations.py
+++ Recursion-2/combinations.py
@@ -15,3 +15,10 @@ class Solution:
         if k == 0 or n < k:
             return [[]]
         return [pre + [i] for i in range(k, n+1) for pre in self.combine(i-1, k-1)]
+
+    # iterative approach
+    def combine(self, n: int, k: int) -> list[list[int]]:
+        combs = [[]]
+        for _ in range(k):
+            combs = [[i] + c for c in combs for i in range(1, c[0] if c else n+1)]
+        return combs
