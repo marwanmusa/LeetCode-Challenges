99d45222bbd028578ef12e1ab9127da1b5133b72
Marwan Musa <marwanlmusa@gmail.com>
2023-11-22 21:11:16 +0700

    add backtrack approach 2

--- Recursion-2/combinations.py
+++ Recursion-2/combinations.py
@@ -57,3 +57,25 @@ class Solution:
                     comb.pop()
         backtrack(k, [], 1)
         return ans
+    
+    # backtrack 2
+    def combine(self, n: int, k: int) -> list[list[int]]:
+        def backtrack(curr, first_num):
+            if len(curr) == k:
+                ans.append(curr[:])
+                return
+
+            need = k - len(curr)
+            remain = n - first_num + 1
+            available = remain - need
+            
+            for num in range(first_num, first_num + available + 1):
+                curr.append(num)
+                backtrack(curr, num + 1)
+                curr.pop()
+
+            return
+        
+        ans = []
+        backtrack([], 1)
+        return ans
