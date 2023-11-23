1c2d540f4c7194d36fb4e14e16a5f6b12b1d87fa
Marwan Musa <marwanlmusa@gmail.com>
2023-11-23 10:52:46 +0700

    add "Unfold Recursion" explanation

---
+++ Recursion-2/unfold_recursion.py
@@ -0,0 +1,55 @@
+# Unfold recursion means convert a recursion algorithm to non-recursion algorithm.
+
+"""
+For example:
+ - Determine if two binary trees are the same or not.
+
+Given two binary trees, write a function to check if they are the same or not.
+Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+"""
+
+from collections import deque
+
+class Solution:
+    # Recursive type
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """    
+        # p and q are both None
+        if not p and not q:
+            return True
+        # one of p and q is None
+        if not q or not p:
+            return False
+        if p.val != q.val:
+            return False
+        return self.isSameTree(p.right, q.right) and \
+               self.isSameTree(p.left, q.left)
+    
+    # Non-recursive type
+    def isSameTree_non_recursive(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        def check(p, q):
+            if not p and not q:
+                return True
+            if not p or not q:
+                return False
+            if p.val != q.val:
+                return False
+            return True
+        stack = deque([(p, q)])
+        while stack:
+            p, q = stack.pop_left()
+            if not check(p, q):
+                return False
+            if p:
+                stack.append((p.right, q.right))
+                stack.append((p.left, q.left))
+        return True
