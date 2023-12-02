69e5f92b35d1e35b6199382d8fd54696d0df0dfe
Marwan Musa <Marwanlmusa@gmail.com>
2023-12-02 21:56:00 +0700

    Add another approach using yield

--- Recursion-2/generate_parentheses.py
+++ Recursion-2/generate_parentheses.py
@@ -11,3 +11,14 @@ class Solution:
             if not right:       parens += p,
             return parens
         return generate('', n, n)
+
+
+    # solution 2
+    def generateParenthesis(self, n: int) -> list[str]:
+        def generate(p, left, right):
+            if right >= left >= 0:
+                if not right: 
+                    yield p
+                for q in generate(p + '(', left-1, right): yield q
+                for q in generate(p + ')', left, right-1): yield q
+        return list(generate('', n, n))
