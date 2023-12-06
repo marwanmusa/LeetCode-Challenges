2f94bcaeab1a011d14e2b8cd3a7ebe7972c871c7
Marwan Musa <marwanlmusa@gmail.com>
2023-12-06 21:53:14 +0700

    add another iterative approach

--- BinaryTree/binary_tree_inorder_traversal.py
+++ BinaryTree/binary_tree_inorder_traversal.py
@@ -70,3 +70,20 @@ class Solution:
                 curr = curr.left
                 temp.left = None
         return res
+    
+
+    # another iterative
+    def inorderTraversal(self, root):
+        ans = []
+        stack = []
+        
+        while stack or root:
+            if root:
+                stack.append(root)
+                root = root.left
+            else:
+                tmpNode = stack.pop()
+                ans.append(tmpNode.val)
+                root = tmpNode.right
+            
+        return ans
