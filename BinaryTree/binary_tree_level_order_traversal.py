cfccca3826ed4d8d2e4d3c44926a9c7fab2eff99
Marwan Musa <marwanlmusa@gmail.com>
2023-12-20 22:03:25 +0700

    add example in the description

--- BinaryTree/binary_tree_level_order_traversal.py
+++ BinaryTree/binary_tree_level_order_traversal.py
@@ -13,6 +13,10 @@ class Solution:
     Task:
     Given the root of a binary tree, return the level order traversal of its nodes' values.
     (i.e., from left to right, level by level).
+
+    Example:
+    Input: root = [3,9,20,null,null,15,7]
+    Output: [[3],[9,20],[15,7]]
     """
     # bfs with stack
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
