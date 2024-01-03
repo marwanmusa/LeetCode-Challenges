33d163b0e709c0eabbe4e79da9b2f748438a911b
Marwan Musa <marwanlmusa@gmail.com>
2024-01-03 21:30:03 +0700

    recursion with backtracking

--- Recursion-2/permutations.py
+++ Recursion-2/permutations.py
@@ -55,4 +55,16 @@ class Solution:
                 res.append([nums[i]] + p)
         return res
     
+    # Method 3 - recursion with bactracking
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        def recursive(nums, perm=[], res=[]):
+            if not nums: res.append(perm)
+            for i in range(len(nums)):
+                sub = nums[:i] + nums [i+1:]
+                perm.append(nums[i])
+                recursive(sub, perm, res)
+                perm.pop()
+            return res
+        return recursive(nums)
+    
     
