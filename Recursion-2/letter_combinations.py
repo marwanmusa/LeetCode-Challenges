31dff19943984316a2a6053e3878469cf3b9800c
Marwan Musa <marwanlmusa@gmail.com>
2024-01-09 11:13:03 +0700

    add 17. Letter Combinations of a Phone Number solution

---
+++ Recursion-2/letter_combinations.py
@@ -0,0 +1,17 @@
+map_num = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', 
+           '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
+
+class Solution:
+    """
+    Task:
+    Given a string containing digits from 2-9 inclusive, return all possible letter combinations
+    that the number could represent. Return the answer in any order.
+
+    A mapping of digits to letters (just like on the telephone buttons) is given below.
+    Note that 1 does not map to any letters.
+    """
+    def letterCombinations(self, digits: str) -> list[str]:
+        comb = [''] if digits else []
+        for d in digits:
+            comb = [p + q for p in comb for q in map_num[d]]
+        return comb
