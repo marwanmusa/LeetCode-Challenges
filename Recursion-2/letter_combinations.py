527b9bedd2d9a7e1b11920bbd0fc5f2529e63b5c
Marwan Musa <marwanlmusa@gmail.com>
2024-01-13 23:27:44 +0700

    add approach using reduce functools

--- Recursion-2/letter_combinations.py
+++ Recursion-2/letter_combinations.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 map_num = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', 
            '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
 
@@ -27,4 +29,8 @@ class Solution:
                 backtrack(i+1, cur+[c])
         backtrack(0, [])
         return res
-        
+
+        # another approach using reduce python
+    def letterCombinations(self, digits: str) -> list[str]:
+        if not digits: return []
+        return reduce(lambda acc, digit: [x+y for x in acc for y in map_num[digit]], digits, [''])
