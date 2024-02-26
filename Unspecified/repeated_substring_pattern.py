0cd76e42ce9ecfd1b1668019c868d2ba3e49b07d
Marwan Musa <marwanlmusa@gmail.com>
2024-02-26 10:25:28 +0700

    add brute force approach

--- Unspecified/repeated_substring_pattern.py
+++ Unspecified/repeated_substring_pattern.py
@@ -11,3 +11,13 @@ class Solution:
     # optimizing prev approach
     def repeatedSubstringPattern(self, s: str) -> bool:
         return (s[1:]+s[:-1]).find(s) >= 0
+    
+    # brute force approach
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        n = len(s)
+        for i in range(1, len(s)//2 + 1):
+            if n % i == 0:
+                sub = s[:i]
+                if sub[:i] * (n // i) == s:
+                    return True
+        return False
