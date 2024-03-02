2c6a587ad15c100eda6882c7cf0648945a4da868
Marwan Musa <marwanlmusa@gmail.com>
2024-03-02 23:51:38 +0700

    add flip bit by bit approach

--- Unspecified/number_complement.py
+++ Unspecified/number_complement.py
@@ -13,3 +13,11 @@ class Solution:
         for bit in bin(num)[2:]:
             revs += bits[bit]
         return int(revs, 2)
+    
+    # flip bit by bit
+    def findComplement(self, num: int) -> int:
+        i = 1
+        while num >= i:
+            num ^= i
+            i <<= 1
+        return num
