67892b29b2b720a7379ee9948ff3e5765ee596a7
Marwan Musa <Marwanlmusa@gmail.com>
2024-03-11 15:02:17 +0700

    Update solution

--- Unspecified/licensing_key_formatting.py
+++ Unspecified/licensing_key_formatting.py
@@ -1,15 +1,12 @@
 class Solution:
     def licenseKeyFormatting(self, s: str, k: int) -> str:
-        res = deque([])
+        s = s.replace('-', '').upper()
+        res = ""
         n = len(s)
-        dash = '-'
-        cur = 0
-        for i in range(n - 1, -1, -1):
-            if s[i] != dash:
-                if cur < k:
-                    res.appendleft((s[i]).upper())
-                    cur += 1
-                elif cur == k-1 and i != 0:
-                    res.appendleft(dash)
-                    cur -= k
-        return "".join(res)
+        remainder = n % k
+        for i in range(remainder, n, k):
+            res += '-' + s[i:i+k]
+        if remainder > 0:
+            return s[:remainder] + res
+        else:
+            return res[1:]
