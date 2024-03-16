264a2014ab9877177e8e5d82eb2e9e528006051a
Marwan Musa <marwanlmusa@gmail.com>
2024-03-16 11:53:51 +0700

    add shorter version

--- Unspecified/construct_the_rectangle.py
+++ Unspecified/construct_the_rectangle.py
@@ -5,3 +5,9 @@ class Solution:
             l = area // i
             if w * l == area:
                 return [l, w]
+
+    # Shorter version
+    def constructRectangle(self, area: int) -> list[int]:
+        for l in range(int(area**0.5), 0, -1):
+            if area % l == 0:
+                return [area // l, l]
