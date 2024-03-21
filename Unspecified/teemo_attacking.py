c6a4284e41e33cbccbe39a4f4b0e41e5ca5cb03d
Marwan Musa <marwanlmusa@gmail.com>
2024-03-21 23:34:45 +0700

    add new approach

--- Unspecified/teemo_attacking.py
+++ Unspecified/teemo_attacking.py
@@ -24,3 +24,15 @@ class Solution:
         for i in range(1, len(timeSeries)):
             ans -= max(0, duration - (timeSeries[i] - timeSeries[i-1]))
         return ans
+
+    def findPoisonedDuration(self, timeSeries: list[int], duration: int) -> int:
+        cur=-1
+        ans=0
+        for i in timeSeries:
+            if i>cur:
+                cur=i+duration-1
+                ans+=duration
+            else: 
+                ans+=(duration-cur+i-1)
+                cur=i+duration-1
+        return ans
