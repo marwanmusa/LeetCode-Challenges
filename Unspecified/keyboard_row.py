6dbf88868442997c823964ee1841d727c8017a03
Marwan Musa <marwanlmusa@gmail.com>
2024-03-28 17:28:22 +0700

    add 500. Keyboard Row solution

---
+++ Unspecified/keyboard_row.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findWords(self, words: list[str]) -> list[str]:
+        row1 = {x : 1 for x in "qwertyuiop"}
+        row2 = {x : 2 for x in "asdfghjkl"}
+        row3 = {x : 3 for x in "zxcvbnm"}
+        row = {**row1, **row2, **row3}
+        ans = []
+        for w in words:
+            check = row[w[0].lower()]
+            oneline = True
+            for i in range(1, len(w)):
+                if row[w[i].lower()] != check:
+                    oneline = False
+                    break
+            if oneline: ans.append(w)
+        return ans
+                
