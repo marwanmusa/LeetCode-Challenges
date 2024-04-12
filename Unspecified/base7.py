1fdf17eff40a041d6ac0e7dc5070a8528dd3526a
Marwan Musa <Marwanlmusa@gmail.com>
2024-04-12 07:11:04 +0700

    Update base7.py

--- Unspecified/base7.py
+++ Unspecified/base7.py
@@ -15,3 +15,15 @@ class Solution:
         if n < 0: return '-' + self.convertToBase7(-n)
         if n < 7: return str(n)
         return self.convertToBase7(n // 7) + str(n % 7)
+
+    # longer version of recursive
+    def convertToBase7(self, num: int) :
+        if num < 0:
+            return f"-{self.convertToBase7(-num)}"
+        elif num < 7:
+            return str(num)
+        res = ""
+        while num > 0:
+            num, r = divmod(num, 7)
+            res += str(r)
+        return res[::-1]
