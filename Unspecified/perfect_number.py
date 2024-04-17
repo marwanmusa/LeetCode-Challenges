8a08fef4d76a59d3e381919c9e399e23c8522ce2
Marwan Musa <marwanlmusa@gmail.com>
2024-04-17 18:05:48 +0700

    add brute-force approach

--- Unspecified/perfect_number.py
+++ Unspecified/perfect_number.py
@@ -8,3 +8,12 @@ class Solution:
     # perfect Number exist until 10^8 (only 5)
     def checkPerfectNumber(self, num: int) -> bool:
         return num in [6,28,496,8128,33550336]
+    
+    # brute-force approach
+    def checkPerfectNumber(self, num: int) -> bool:
+        ans = 1
+        if num == 1: return False
+        for i in range(2,int(num**0.5)+1):
+            if num % i == 0:
+                ans += i + num // i
+        return ans == num
