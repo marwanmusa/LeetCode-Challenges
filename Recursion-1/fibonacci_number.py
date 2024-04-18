18149460cfb574dca9afdead107b5769869adfb0
Marwan Musa <marwanlmusa@gmail.com>
2024-04-18 09:49:52 +0700

    add pre-define fib-list approach

--- Recursion-1/fibonacci_number.py
+++ Recursion-1/fibonacci_number.py
@@ -32,3 +32,12 @@ class Solution:
             cache[n] = result
             return result
         return recur_fib(n)
+    
+    # pre-define the fib-list
+    def fib(self, n):
+        if n < 2: return n
+        fib_list = [0] * (n+1)
+        fib_list[1] = 1
+        for i in range(2, n+1):
+            fib_list[i] = fib_list[i-1] + fib_list[i-2]
+        return fib_list[n]
