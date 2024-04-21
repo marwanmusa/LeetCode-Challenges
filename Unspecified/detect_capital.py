61c6500e7c350e66361a128b9a6e0a919bc90a9e
Marwan Musa <marwanlmusa@gmail.com>
2024-04-21 20:32:29 +0700

    make the code shorter

--- Unspecified/detect_capital.py
+++ Unspecified/detect_capital.py
@@ -3,9 +3,5 @@ class Solution:
     def detectCapitalUse(self, word: str) -> bool:
         up = re.findall("[A-Z]", word)
         low = re.findall("[a-z]", word)
-        if len(up) == 1 and up[0] == word[0]:
-            return True
-        if not up or not low:
-            return True
-        return False
+        return True if (len(up) == 1 and up[0] == word[0]) or (not up or not low) else False
             
