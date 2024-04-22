24421ee966edab4ac8ff511c31487dc88c268f1d
Marwan Musa <marwanlmusa@gmail.com>
2024-04-22 13:03:31 +0700

    using string methods approach

--- Unspecified/detect_capital.py
+++ Unspecified/detect_capital.py
@@ -5,3 +5,6 @@ class Solution:
         low = re.findall("[a-z]", word)
         return True if (len(up) == 1 and up[0] == word[0]) or (not up or not low) else False
             
+    # using string methods
+    def detectCapitalUse(self, word: str) -> bool:
+        return True if word.isupper() or word.islower() or word.title() == word else False
