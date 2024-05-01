0fb0273b00002ad9d59dc075b7e3649ad7ac9742
Marwan Musa <marwanlmusa@gmail.com>
2024-05-01 21:17:51 +0700

    add 541. Reverse String II solution

---
+++ Array&String/reverse_string2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        s = list(s)
+        m = len(s) % (k*2)
+        for i in range(0, len(s), k*2):
+            if i + k < len(s):
+                s[i:i+k] = s[i:i+k][::-1]
+            else:
+                s[i:] = s[i:][::-1]
+        return ''.join(s)
+    
+    # most efficient implementation
+    def reverseStr(self, s: str, k: int) -> str:
+        i, revStr = 0, ""
+        while i < len(s):
+            revStr += s[i:i+k][::-1]
+            revStr += s[i+k:i+(2*k)]
+            i += 2*k
+        return revStr
