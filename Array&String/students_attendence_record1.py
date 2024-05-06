7bdda997d479c00ec937a3024ec65f853a8b1ca4
Marwan Musa <marwanlmusa@gmail.com>
2024-05-06 19:08:47 +0700

    add shorter version

--- Array&String/students_attendence_record1.py
+++ Array&String/students_attendence_record1.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 class Solution:
     # brute force
     def checkRecord(self, s: str) -> bool:
@@ -5,3 +7,7 @@ class Solution:
         for a in s:
             if a == 'A': allA += 1        
         return allA < 2 and s.find("LLL") == -1
+    
+    # shorter version
+    def checkRecord(self, s: str) -> bool:
+        return Counter(s)['A'] < 2 and s.find("LLL") == -1
