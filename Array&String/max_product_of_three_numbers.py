6096da41a82121c84812e882404160b54e0bbfa2
Marwan Musa <marwanlmusa@gmail.com>
2024-05-25 15:12:18 +0700

    update solution

--- Array&String/max_product_of_three_numbers.py
+++ Array&String/max_product_of_three_numbers.py
@@ -5,8 +5,16 @@ class Solution:
         if len(nums) == 3:
             return math.prod(nums)
         all_max = []
+        min_neg = []
+        all_neg = [x for x in nums if x < 0]
+        if len(all_neg) >= 2:
+            for i in range(2):
+                x = min(all_neg)
+                all_neg.remove(x)
+                min_neg.append(x)
+        arr = nums[::]
         for i in range(3):
-            x = max(nums)
-            nums.remove(x)
+            x = max(arr)
+            arr.remove(x)
             all_max.append(x)
-        return math.prod(all_max)
+        return max(math.prod(all_max), math.prod(min_neg)*max(nums)) if min_neg else math.prod(all_max) 
