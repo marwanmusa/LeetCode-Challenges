808817fbd9ea4407f7e8b9b821cb0ac78857ef03
Marwan Musa <marwanlmusa@gmail.com>
2024-05-29 16:03:43 +0700

    add variations

--- Array&String/robot_return_to_origin.py
+++ Array&String/robot_return_to_origin.py
@@ -5,3 +5,11 @@ class Solution:
         for w in moves:
             d[w] += 1
         return True if d['U'] - d['D'] == 0 and d['L'] - d['R'] == 0 else False
+    
+    # variation 2
+    def judgeCircle(self, moves: str) -> bool:
+        return not any([moves.count(a[0]) - moves.count(a[1]) for a in ["LR", "UD"]])
+    
+    # variation 3
+    def judgeCircle(self, moves: str) -> bool:
+        return all([moves.count(a[0]) == moves.count(a[1]) for a in ["LR", "UD"]])
