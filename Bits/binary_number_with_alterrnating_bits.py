699a1314ca68a4701f7f92901b6732e74121cf01
Marwan Musa <marwanlmusa@gmail.com>
2024-06-08 22:18:07 +0700

    add manual checking approach

--- Bits/binary_number_with_alterrnating_bits.py
+++ Bits/binary_number_with_alterrnating_bits.py
@@ -2,3 +2,15 @@ class Solution:
     #1 finding the repeated 0 or 1 in bit string n
     def hasAlternatingBits(self, n: int) -> bool:
         return '11' not in bin(n) and '00' not in bin(n)
+
+    #2 manually check
+    def hasAlternatingBits(self, n: int) -> bool:
+        last = n % 2
+        n //= 2
+        while n > 0:
+            cur = n % 2
+            if last == cur:
+                return False
+            last = cur
+            n //= 2
+        return True
