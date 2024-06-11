3284dee7534c4f6a9a2908c2b07a32be3c13b824
Marwan Musa <marwanlmusa@gmail.com>
2024-06-11 17:26:15 +0700

    update space complexity to O(1)

--- Array&String/count_binary_strings.py
+++ Array&String/count_binary_strings.py
@@ -11,3 +11,16 @@ class Solution:
         for i in range(1, len(chunks)):
             res += min(chunks[i], chunks[i-1])
         return res
+    
+    # O(1) space
+    def countBinarySubstrings(self, s: str) -> int:
+        res, prev, cur = 0, 0, 1
+        for i in range(1, len(s)):
+            if s[i] == s[i-1]:
+                cur += 1
+            else:
+                res += min(prev, cur)
+                prev = cur
+                cur = 1
+        res += min(prev, cur)
+        return res
