47255f985c4989fff6dbffc00c4bed1dabf361f8
Marwan Musa <marwanlmusa@gmail.com>
2024-06-24 11:26:51 +0700

    add solution using bit shifting

--- Bits/prime_number_of_set_bits_in_binary_representation.py
+++ Bits/prime_number_of_set_bits_in_binary_representation.py
@@ -2,4 +2,8 @@ from collections import Counter
 class Solution:
     def countPrimeSetBits(self, left: int, right: int) -> int:
         prs = {x: True for x in [2, 3, 5, 7, 11, 13, 17, 19]}
-        return sum([prs.get(Counter(filter(lambda x: x=='1', bin(x)))['1'], False) for x in range(left, right+1)])
+        return sum([prs.get(bin(x).count('1'), False) for x in range(left, right+1)])
+
+    # using bit shifting
+    def countPrimeSetBits(self, L, R):
+        return sum(665772 >> bin(i).count('1') & 1 for i in range(L, R+1))
