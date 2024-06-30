7bc29bd8e39fd1319055ded3bdd382e507a44a8e
Marwan Musa <marwanlmusa@gmail.com>
2024-06-30 16:16:00 +0700

    make the code more efficient

--- Array&String/unique_morse_code_words.py
+++ Array&String/unique_morse_code_words.py
@@ -1,5 +1,4 @@
 class Solution:
     def uniqueMorseRepresentations(self, words: list[str]) -> int:
         codes = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
-        morse = {chr(i+97): codes[i] for i in range(len(codes))}
-        return len(set(''.join([morse[x] for x in w]) for w in words))
+        return len({''.join(codes[ord(x) - ord('a')] for x in w) for w in words})
