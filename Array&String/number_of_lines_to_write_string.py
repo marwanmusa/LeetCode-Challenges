5ea04ea9696799cbe1dc689d6b4f51e9f7aea074
Marwan Musa <marwanlmusa@gmail.com>
2024-07-02 12:58:04 +0700

    add similiar sol to prev sol

--- Array&String/number_of_lines_to_write_string.py
+++ Array&String/number_of_lines_to_write_string.py
@@ -8,3 +8,12 @@ class Solution:
                 last = 0
             last += cur
         return [lines+1, last]
+
+    # similar to prev solution
+    def numberOfLines(self, widths: list[int], s: str) -> list[int]:
+        last, lines = 0, 1
+        for w in s:
+            cur = widths[ord(w)-ord('a')]
+            lines += 1 if last + cur > 100 else 0
+            last = cur if last + cur > 100 else last + cur
+        return [lines, last]
