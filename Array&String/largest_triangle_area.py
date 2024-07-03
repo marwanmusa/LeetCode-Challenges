7c6ede18e98c70295505df13685ff17393500b16
Marwan Musa <marwanlmusa@gmail.com>
2024-07-03 23:51:34 +0700

    update solution

--- Array&String/largest_triangle_area.py
+++ Array&String/largest_triangle_area.py
@@ -1,4 +1,6 @@
+import itertools
+
 class Solution:
-    def largestTriangleArea(self, points: List[List[int]]) -> float:
+    def largestTriangleArea(self, points: list[list[int]]) -> float:
         return max(0.5 * abs(i[0]*j[1] + j[0]*k[1] + k[0]*i[1] - i[0]*k[1] - k[0]*j[1] - j[0]*i[1])
                    for i, j, k in itertools.combinations(points, 3))
