530495531440b8bd05419fc087800f8ef71f5f02
Marwan Musa <marwanlmusa@gmail.com>
2024-07-06 23:44:04 +0700

    add oneliner solution

--- Array&String/most_common_word.py
+++ Array&String/most_common_word.py
@@ -1,4 +1,5 @@
-from collections import defaultdict
+import re
+from collections import defaultdict, Counter
 
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: list[str]) -> str:
@@ -9,3 +10,13 @@ class Solution:
                 d[w] += 1
         print(d)
         return max(zip(d.values(), d.keys()))[1]
+    
+    # shorter
+    def mostCommonWord(self, paragraph: str, banned: list[str]) -> str:
+        paragraph = paragraph.lower().replace('!', ' ').replace('?', ' ').replace("'", ' ').replace(',', ' ').replace(';', ' ').replace('.', ' ')
+        d = {k: v for k, v in Counter(paragraph.split()).items() if k not in banned}
+        return max(zip(d.values(), d.keys()))[1]
+    
+    # one liner
+    def mostCommonWord(self, p: str, banned: list[str]) -> str:
+        return Counter(w for w in re.findall(r'\w+', p.lower()) if w not in set(banned)).most_common(1)[0][0]
