1a39c8ea73d5a3297791f58459d46b6d939eb8cd
Marwan Musa <marwanlmusa@gmail.com>
2024-07-11 09:53:44 +0700

    add final solution

--- Array&String/shortest_distance_to_a_char.py
+++ Array&String/shortest_distance_to_a_char.py
@@ -6,16 +6,22 @@ class Solution:
     The distance between two indices i and j is abs(i - j), where abs is the absolute value function.
     """
     def shortestToChar(self, s: str, c: str) -> list[int]:
-        loc = []
+        last = -1
+        op = False
         ans = []
         for i, e in enumerate(s):
-            if e == c: loc.append(i)
-        if len(loc) == 1:
-            ans += range(loc[0], -1 , -1)
-            ans += range()
-        return []
+            if e == c: 
+                if last < 0:
+                    ans += range(i, -1, -1)
+                else:
+                    ans += self.create_pascal(i - last + 1)[1:]
+                last = i
+        if i == len(s)-1 and i != last:
+            ans += range(1, i-last+1)
+        return ans
+            
 
-    def create_pascal(n):
+    def create_pascal(self, n):
         dp = [0] * n
         if n & 1:
             for i in range(1, n):
