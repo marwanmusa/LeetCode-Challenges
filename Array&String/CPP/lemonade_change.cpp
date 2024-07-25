1f250a1150ab08ae758b6333af53ea8adae34da2
Marwan Musa <marwanlmusa@gmail.com>
2024-07-25 21:18:10 +0700

    update 860. Lemonade Change solution

--- Array&String/CPP/lemonade_change.cpp
+++ Array&String/CPP/lemonade_change.cpp
@@ -1,20 +1,38 @@
 #include <vector>
+#include <map>
 using namespace std;
 
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        int change = 0;
-        for (int i : bills) {
-            if (i == 5) change += i;
+        map<int, int> change{
+            {5, 0},
+            {10, 0},
+            {20, 0},
+        };
+        for (int n : bills) {
+            change[n] += 1;
+            if (n == 5) continue;
+            if (n == 10) {
+                if (change[5] == 0) return false;
+                else change[5] -= 1;
+            }
             else {
-                if ((i - 5) <= change) {
-                    change -= (i - 5);
-                    change += 5;
+                if (change[5] == 0) return false;
+                else {
+                    if (change[10] == 0) {
+                        if (change[5] >= 3) {
+                            change[5] -= 3;
+                            continue;
+                            }
+                        else return false;
+                    }
+                    change[10] -= 1;
+                    change[5] -= 1;
                 }
-                else return false;
             }
         }
+        
         return true;
     }
 };
