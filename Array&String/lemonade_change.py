5e7f58e8accb467db510a40bee4c6ddb82388e20
Marwan Musa <marwanlmusa@gmail.com>
2024-07-28 23:44:30 +0700

    improve typing

--- Array&String/lemonade_change.py
+++ Array&String/lemonade_change.py
@@ -4,7 +4,8 @@ class Solution:
     def lemonadeChange(self, bills: list[int]) -> bool:
         five, ten = 0, 0
         for bill in bills:
-            if bill == 5: five += 1
+            if bill == 5:
+                five += 1
             elif bill == 10 and five: 
                 five -= 1; ten += 1
             elif bill == 20 and ten and five:
