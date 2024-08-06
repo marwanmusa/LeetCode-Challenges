eedfe1f735ed2acfebf294f3597ad50e0f4db217
Marwan Musa <marwanlmusa@gmail.com>
2024-08-06 13:39:29 +0700

    add shorter version

--- Array&String/CPP/uncommon_words_from_two_sentences.cpp
+++ Array&String/CPP/uncommon_words_from_two_sentences.cpp
@@ -1,4 +1,5 @@
 #include <map>
+#include <sstream>
 #include <string>
 #include <vector>
 using namespace std;
@@ -6,33 +7,13 @@ using namespace std;
 class Solution {
 public:
     vector<string> uncommonFromSentences(string s1, string s2) {
-        s1 += ' ';
-        s2 += ' ';
-        string temp;
-        map<string, int> cs1, cs2;
+        istringstream s(s1 + " " + s2);
+        map<string, int> cs;
+        while (s >> s1) cs[s1]++;
         vector<string> res;
-        addToCounter(s1, cs1);
-        addToCounter(s2, cs2);
-        countRes(cs1, cs2, res);
-        countRes(cs2, cs1, res);
+        for (auto& k: cs) {
+            if (k.second == 1 ) res.push_back(k.first);
+        }
         return res;
-    }
-
-    void addToCounter(string& s, map<string, int>& counter) {
-        string temp;
-        for (char e: s) {
-            if (e == ' ') {
-                counter[temp] += 1;
-                temp = "";
-            } else {
-                temp.push_back(e);
-            }
-        }
-    }
-
-    void countRes(map<string, int>& counter1, map<string, int>& counter2, vector<string>& res) {
-        for (auto& k: counter1) {
-            if (k.second == 1 && counter2.find(k.first) == counter2.end()) res.push_back(k.first);
-        }
     }
 };
--- Array&String/uncommon_words_from_two_sentences.py
+++ Array&String/uncommon_words_from_two_sentences.py
@@ -2,13 +2,9 @@ from collections import Counter
 
 class Solution:
     def uncommonFromSentences(self, s1: str, s2: str) -> list[str]:
-        cs1 = Counter(s1.split())
-        cs2 = Counter(s2.split())
+        cs = Counter((s1 + " " + s2).split())
         ans = []
-        for k in cs1:
-            if cs1[k] == 1 and not cs2.get(k):
-                ans.append(k)
-        for k in cs2:
-            if cs2[k] == 1 and not cs1.get(k):
+        for k in cs:
+            if cs[k] == 1:
                 ans.append(k)
         return ans
