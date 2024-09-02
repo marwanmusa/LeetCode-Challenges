03c80bfa25e3c951672238646fcd17f2b03d6ba3
Marwan Musa <marwanlmusa@gmail.com>
2024-09-02 10:33:04 +0700

    add shorter approach

--- BinarySearch/CPP/range_sum_of_bst.cpp
+++ BinarySearch/CPP/range_sum_of_bst.cpp
@@ -16,4 +16,10 @@ public:
         else if (root->val > high) return rangeSumBST(root->left, low, high);
         return root->val + rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high);
     }
+
+    // shorter
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if (!root) return 0;
+        return (low <= root->val && root->val <= high? root->val : 0) + rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high);
+    }
 };
