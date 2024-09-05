67d6b1c184fc7d5d8735bd74b5511aebabca63c0
Marwan Musa <marwanlmusa@gmail.com>
2024-09-05 23:53:42 +0700

    add another approach

--- TwoPointers/CPP/di_string_match.cpp
+++ TwoPointers/CPP/di_string_match.cpp
@@ -19,4 +19,11 @@ public:
         ans.push_back(i);
         return ans;
     }
+
+    vector<int> diStringMatch(string S) {
+        vector<int> res;
+        for (int l = 0, h = S.size(), i = 0; i <= S.size(); ++i)
+            res.push_back(i == S.size() || S[i] == 'I' ? l++ : h--);
+    return res;
+    }
 };
