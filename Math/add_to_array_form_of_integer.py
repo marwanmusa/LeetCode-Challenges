6a1e2858bbeb180ce55cb5f618f7cb17364a0d40
Marwan Musa <marwanlmusa@gmail.com>
2024-09-22 15:24:35 +0700

    add 989. Add to Array-Form of Integer solution

---
+++ Math/add_to_array_form_of_integer.py
@@ -0,0 +1,28 @@
+class Solution:
+    def addToArrayForm(self, num: List[int], k: int) -> List[int]:
+        n, numint, idx = len(num) - 1, 0, 0
+        numint = 0
+        while n >= 0:
+            numint += ((10 ** n) * num[idx])
+            idx += 1
+            n -= 1
+        numint += k
+
+        ans = deque()
+        while numint > 0:
+            ans.appendleft(numint % 10)
+            numint //= 10
+        return ans
+
+    # more-efficient
+    def addToArrayForm(self, num: List[int], k: int) -> List[int]:
+        i = len(num) - 1
+        while k > 0:
+            if i >= 0:
+                k = num[i]
+                num[i] = k % 10
+                i -= 1
+            else:
+                num.insert(0, k % 10)
+            k //= 10
+        return num
