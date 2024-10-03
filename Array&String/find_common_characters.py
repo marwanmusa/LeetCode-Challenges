2b25cb739dfabaf010163e50fb8727ef62ec0316
Marwan Musa <marwanlmusa@gmail.com>
2024-10-03 10:18:16 +0700

    add one-liner sol

--- Array&String/find_common_characters.py
+++ Array&String/find_common_characters.py
@@ -1,4 +1,6 @@
 from collections import Counter
+from functools import reduce
+from operator import and_
 
 class Solution:
     def commonChars(self, words: list[str]) -> list[str]:
@@ -18,3 +20,7 @@ class Solution:
         for a in A:
             res &= Counter(a)
         return list(res.elements())
+
+    # one-liner
+    def commonChars(self, A: list[str]) -> list[str]:
+        return reduce(and_, map(Counter, A)).elements()
