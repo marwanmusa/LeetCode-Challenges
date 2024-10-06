cbb3b7fecbe307dc04f0499a6aa1220e00352827
Marwan Musa <marwanlmusa@gmail.com>
2024-10-06 22:18:39 +0700

    update sol

--- Array&String/Sorting/CPP/maximize_sum_of_array_after_k_negations.cpp
+++ Array&String/Sorting/CPP/maximize_sum_of_array_after_k_negations.cpp
@@ -1,13 +1,17 @@
 #include <vector>
 #include <algorithm>
+#include <numeric>
 using namespace std;
+
 class Solution {
 public:
     int largestSumAfterKNegations(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-        for (int i = 0; i < k; i++) {
-            nums[i] = -nums[i];
+        vector<int> negs;
+        for (int x: nums) {
+            if (x < 0) negs.push_back(x);
         }
+        sort(negs.begin(), negs.end());
+
         return accumulate(nums.begin(), nums.end(), 0);
     }
 };
