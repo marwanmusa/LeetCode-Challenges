2fff9a3e661503e689ed2156f6c801afd8dd258c
Marwan Musa <marwanlmusa@gmail.com>
2024-10-09 17:19:18 +0700

    add shorter solution 1005. Maximize Sum Of Array After K Negations

--- Array&String/Sorting/CPP/maximize_sum_of_array_after_k_negations.cpp
+++ Array&String/Sorting/CPP/maximize_sum_of_array_after_k_negations.cpp
@@ -26,4 +26,13 @@ public:
         if (k && k % 2)  ans[0] = -ans[0];
         return accumulate(ans.begin(), ans.end(), 0);
     }
+
+    // shorter
+    int largestSumAfterKNegations(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        for (int i = 0; k > 0 && i < nums.size() && nums[i] < 0; ++i, --k) {
+            nums[i] = -nums[i];
+        }
+        return accumulate(nums.begin(), nums.end(), 0) - ((k % 2) * *min_element(nums.begin(), nums.end()) * 2);
+    }
 };
--- Array&String/Sorting/maximize_sum_of_array_after_k_negations.py
+++ Array&String/Sorting/maximize_sum_of_array_after_k_negations.py
@@ -9,3 +9,13 @@ class Solution:
         smalls = sorted(smalls + pos)
         if k and k % 2: smalls[0] = -smalls[0]
         return sum(smalls)
+
+
+    # shorter
+    def largestSumAfterKNegations(self, A: list[int], K: int) -> int:
+        A.sort()
+        i = 0
+        while i < len(A) and i < K and A[i] < 0:
+            A[i] = -A[i]
+            i += 1
+        return sum(A) - (K - i) % 2 * min(A) * 2
