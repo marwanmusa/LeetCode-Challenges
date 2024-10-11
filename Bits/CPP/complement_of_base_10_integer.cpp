5e01c9732315ca98ea29c9d77a582e79ff07961a
Marwan Musa <marwanlmusa@gmail.com>
2024-10-11 23:54:58 +0700

    add shorter solution using bit masking

--- Bits/CPP/complement_of_base_10_integer.cpp
+++ Bits/CPP/complement_of_base_10_integer.cpp
@@ -34,4 +34,14 @@ public:
         }
         return res;
     }
+
+    // shorter
+    int bitwiseComplement(int N) {
+        int mask = 1;
+
+        while (mask < N)
+        mask = (mask << 1) + 1;
+
+        return mask ^ N;
+    }
 };
