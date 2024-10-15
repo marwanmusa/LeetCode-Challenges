40d025c153c2a9a47ce779bef0208fd4f4f82eba
Marwan Musa <marwanlmusa@gmail.com>
2024-10-15 19:57:21 +0700

    update solution

--- Array&String/GreedyProblem/partition_array_into_three_parts_with_equal_sum.py
+++ Array&String/GreedyProblem/partition_array_into_three_parts_with_equal_sum.py
@@ -1,4 +1,12 @@
 class Solution:
     def canThreePartsEqualSum(self, arr: list[int]) -> bool:
-        #initiation
-        return True
+        sum_arr = sum(arr)
+        if sum_arr % 3: return False
+        if all(v == 0 for v in arr): return True
+        part_n, cur = 3, arr[0]
+        for i in range(1, len(arr)):
+            cur += arr[i]
+            if cur == (sum_arr // 3):
+                cur = 0
+                part_n -= 1
+        return part_n == 0
