f685bb55e9bff8ecb382b603f076e66574805daf
Marwan Musa <marwanlmusa@gmail.com>
2024-10-16 23:59:15 +0700

    add 1013. Partition Array Into Three Parts With Equal Sum solution

--- Array&String/GreedyProblem/partition_array_into_three_parts_with_equal_sum.py
+++ Array&String/GreedyProblem/partition_array_into_three_parts_with_equal_sum.py
@@ -3,9 +3,11 @@ class Solution:
         sum_arr = sum(arr)
         if sum_arr % 3: return False
         if all(v == 0 for v in arr): return True
-        part_n, cur = 3, arr[0]
-        for i in range(1, len(arr)):
+        part_n, cur = 3, 0
+        for i in range(len(arr)):
             cur += arr[i]
+            if part_n == 1 and i < len(arr) - 1:
+                continue
             if cur == (sum_arr // 3):
                 cur = 0
                 part_n -= 1
