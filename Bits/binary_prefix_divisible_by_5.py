cdbc95ced73cb82c0a08d9397a9c3ab3ceaf69b7
Marwan Musa <marwanlmusa@gmail.com>
2024-10-20 00:31:04 +0700

    add shorter solution

--- Bits/binary_prefix_divisible_by_5.py
+++ Bits/binary_prefix_divisible_by_5.py
@@ -1,11 +1,18 @@
 class Solution:
     def prefixesDivBy5(self, nums: list[int]) -> list[bool]:
-        res = [nums[0] % 5 == 0]
+        res = [not nums[0]]
         cur = nums[0]
         n = len(nums)
         if n > 1:
             for i in range(1, n):
-                temp = (2**i) + nums[i]
-                cur += temp
-                res.append(cur % 5 == 0)
+                cur = cur * 2 + nums[i]
+                res.append(not cur % 5)
         return res
+
+    # shorter
+    def prefixesDivBy5(self, nums: list[int]) -> list[bool]:
+        cur, nums[0] = nums[0], nums[0] == 0
+        for i in range(1, len(nums)):
+            cur = (cur * 2 + nums[i])
+            nums[i] = not cur % 5
+        return nums
