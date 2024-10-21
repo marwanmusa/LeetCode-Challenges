bb34a723ae49896c34b6c6dff8aec53d508be3c1
Marwan Musa <marwanlmusa@gmail.com>
2024-10-21 14:10:28 +0700

    add shorter solution

--- Array&String/GreedyProblem/CPP/partition_array_into_three_parts_with_equal_sum.cpp
+++ Array&String/GreedyProblem/CPP/partition_array_into_three_parts_with_equal_sum.cpp
@@ -18,4 +18,17 @@ public:
         }
         return parts == 0;
     }
+
+    // Shorter
+    bool canThreePartsEqualSum(vector<int>& A) {
+        int sum = accumulate(A.begin(), A.end(), 0), part = 0, average = sum / 3, cnt = 0;
+        for (int a : A) {
+            part += a;
+            if (part == average) { // find the average: sum / 3
+                ++cnt; // find an average, increase the counter.
+                part = 0; // reset part.
+            }
+        }
+        return cnt >= 3 && sum % 3 == 0;
+    }
 };
--- Array&String/GreedyProblem/partition_array_into_three_parts_with_equal_sum.py
+++ Array&String/GreedyProblem/partition_array_into_three_parts_with_equal_sum.py
@@ -12,3 +12,32 @@ class Solution:
                 cur = 0
                 part_n -= 1
         return part_n == 0
+
+    # shorter
+    def canThreePartsEqualSum(self, A: list[int]) -> bool:
+        average, remainder, part, cnt = sum(A) // 3, sum(A) % 3, 0, 0
+        for a in A:
+            part += a
+            if part == average:
+                cnt += 1
+                part = 0
+        return not remainder and cnt >= 3
+
+    # greedy pointers
+    def canThreePartsEqualSum(self, A: list[int]) -> bool:
+        l, r, s = 1, len(A) - 2, sum(A)
+        if s % 3 != 0:
+            return False
+        leftSum, rightSum, average = A[0], A[-1], s // 3
+        while l <= r:
+            if l < r and leftSum != average:
+                leftSum += A[l]
+                l += 1
+            if l < r and rightSum != average:
+                rightSum += A[r]
+                r -= 1
+            if leftSum == average == rightSum:
+                return True
+            if l == r:
+                return False
+        return False
