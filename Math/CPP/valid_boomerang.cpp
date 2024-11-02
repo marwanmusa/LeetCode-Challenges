3f5633288393cba95a2edca16a30d8e02c366c0b
Marwan Musa <Marwanlmusa@gmail.com>
2024-11-02 22:40:25 +0700

    Update valid_boomerang.cpp to more cpp style

--- Math/CPP/valid_boomerang.cpp
+++ Math/CPP/valid_boomerang.cpp
@@ -5,6 +5,6 @@ using namespace std;
 class Solution {
 public:
     bool isBoomerang(vector<vector<int>>& pos) {
-        return 0.5 * abs(pos[0][0] * (pos[1][1] - pos[2][1]) + pos[1][0] * (pos[2][1] - pos[0][1]) + pos[2][0] * (pos[0][1] - pos[1][1])) > 0;
+        return (0.5 * abs(pos[0][0] * (pos[1][1] - pos[2][1]) + pos[1][0] * (pos[2][1] - pos[0][1]) + pos[2][0] * (pos[0][1] - pos[1][1])) > 0);
     }
 };
