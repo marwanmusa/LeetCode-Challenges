54b6321efda7ae0e372689f8763f43a9fd86b6b1
Marwan Musa <marwanlmusa@gmail.com>
2024-11-25 14:05:01 +0700

    Update solution

--- Math/CPP/day_of_the_year.cpp
+++ Math/CPP/day_of_the_year.cpp
@@ -6,13 +6,13 @@ class Solution {
 public:
     int dayOfYear(string date) {
         // Days in each month
-        int days_freq[13] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        int days_freq[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         int year = stoi(date.substr(0, 4)),
             month = stoi(date.substr(5, 2)),
             day = stoi(date.substr(8, 2));
         if (isLeapYear(year)) days_freq[2]++;
         int sum = 0;
-        for (int i = 0; i < month - 1; i++) sum += days_freq[i];
+        for (int i = 1; i < month; i++) sum += days_freq[i];
         return sum + min(sum, days_freq[month]);
     }
 
