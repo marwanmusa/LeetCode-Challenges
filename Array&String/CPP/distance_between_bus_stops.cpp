c2ee042c4e8e1d8436c8e591255f34ce04fc6aa3
Marwan Musa <marwanlmusa@gmail.com>
2024-11-28 23:51:01 +0700

    add shorter version of the function

--- Array&String/CPP/distance_between_bus_stops.cpp
+++ Array&String/CPP/distance_between_bus_stops.cpp
@@ -41,4 +41,20 @@ public:
         rsum = accumulate(rslice.begin(), rslice.end(), 0);
         return min(midsum, lsum + rsum);
     }
+
+    int distanceBetweenBusStops(vector<int>& distance, int start, int destination) {
+        int dist_clock = 0, dist_aclock = 0, size = distance.size();
+        int start1 = min(start, destination), destination1 = max(start, destination);
+
+        // Iterate through the array of distances to calculate clockwise and counterclockwise distances
+        for (int i = 0; i < size; i++) {
+            if (i >= start1 && i < destination1)
+                dist_clock += distance[i]; // Accumulate distance for clockwise direction
+            else
+                dist_aclock += distance[i]; // Accumulate distance for counterclockwise direction
+        }
+
+        // Return the minimum distance between clockwise and counterclockwise paths
+        return min(dist_clock, dist_aclock);
+    }
 };
