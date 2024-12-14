9bc3b05d30753d441d218650cff3ab2496f0dd2d
Marwan Musa <marwanlmusa@gmail.com>
2024-12-14 23:51:26 +0700

    add 1266. Minimum Time Visiting All Points solution

--- Math/minimum_time_visiting_all_points.py
+++ Math/minimum_time_visiting_all_points.py
@@ -1,3 +1,15 @@
 class Solution:
+
+    def helper(self, i, j):
+        [a, b], [c, d] = i, j
+        shortDistanceXY = min(abs(a-c), abs(b-d))
+        longDistannceXY = max(abs(a-c), abs(b-d))
+        return shortDistanceXY + (longDistannceXY - shortDistanceXY)
+
     def minTimeToVisitAllPoints(self, points: list[list[int]]) -> int:
-        
+        n = len(points)
+        ans = 0
+        if n == 1: return 0
+        for i in range(n - 1):
+            ans += self.helper(points[i], points[i+1])
+        return ans
