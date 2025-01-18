a99c337f8f04ce0359370b62d7f5894d6155079f
Marwan Musa <marwanlmusa@gmail.com>
2025-01-18 23:26:08 +0700

    add 1380. Lucky Numbers in a Matrix solution

---
+++ Array&String/lucky_numbers_in_a_matrix.py
@@ -0,0 +1,13 @@
+class Solution:
+    def luckyNumbers(self, matrix: list[list[int]]) -> list[int]:
+        r, c = len(matrix), len(matrix[0])
+        minr = {i : arr.index(min(arr)) for i, arr in enumerate(matrix)}
+        maxc = {}
+        for col in range(c):
+            mat = [arr[col] for arr in matrix]
+            maxc[col] = mat.index(max(mat))
+        ans = []
+        for k in minr:
+            if maxc.get(minr[k]) == k:
+                ans.append(matrix[k][minr[k]])
+        return ans
