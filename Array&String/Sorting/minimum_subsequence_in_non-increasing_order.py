857abf67b10df1d317943bde014b424e129de1ac
Marwan Musa <marwanlmusa@gmail.com>
2025-01-26 23:58:42 +0700

    refactor minSubsequence method for improved logic and readability

--- Array&String/Sorting/minimum_subsequence_in_non-increasing_order.py
+++ Array&String/Sorting/minimum_subsequence_in_non-increasing_order.py
@@ -1,10 +1,10 @@
 class Solution:
     def minSubsequence(self, nums: list[int]) -> list[int]:
         if len(nums) == 1: return nums
-        nums.sort(reverse = True)
-        res, total, l = [nums[0]], sum(nums), nums[0]
-        for i in range(1, len(nums)):
-            if (l > total - l) : break
-            l += nums[i]
+        nums.sort()
+        res, total, r = [], sum(nums), 0
+        for i in range(len(nums)-1, -1, -1):
+            if (r > total - r) : break
+            r += nums[i]
             res.append(nums[i])
         return res
