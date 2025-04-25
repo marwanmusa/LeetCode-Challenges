47c26d1c64c1c19263b6a69702a85ea07e378dee
Marwan Musa <marwanlmusa@gmail.com>
2025-04-25 13:54:19 +0700

    fix mistype

--- Array&String/Python/matrix_diagonal_sum.py
+++ Array&String/Python/matrix_diagonal_sum.py
@@ -1,5 +1,5 @@
 class Solution:
-    def diagonalSum(self, mat: List[List[int]]) -> int:
+    def diagonalSum(self, mat: list[list[int]]) -> int:
         ans, odd = 0, 1 & len(mat)
         mid = len(mat) // 2 if 1 & len(mat) else 0
         for i, sub in enumerate(mat):
